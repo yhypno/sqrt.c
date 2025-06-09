a54d8d6671b92c0987eeb05a20226752c2a64503
yhypno <yashwantbora119@gmail.com>
2025-06-09 20:08:01 +0530

    feat: added precision support

--- sqrt.c
+++ sqrt.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-float Sqrt(float a, float b)
+float Sqrt(float a)
 {
+
 	float x,y;
+	float b = 1e-15;
 
 	if (a  < 0) {
 		printf("There is no Sqaure root of a negative Number\n");	
@@ -12,7 +14,7 @@ float Sqrt(float a, float b)
 		x = a;
 		y = a * 0.5;
 
-		while ((x - y) * (x - y) > (b*b)) {
+		while ((x - y) * (x - y) > b) {
 			x = y;
 			y = 0.5 * (x + a/x) ;
 		}
@@ -27,8 +29,14 @@ int main()
 
 	printf("Enter ending number number:(eg. 1.00, 10.0)\n");
 	scanf("%f",&i);
+
+	int pre;
+	printf("Enter the precision:(eg 4,5,6;)\n");
+	scanf("%d",&pre);
+	
+
 	for (float j = 1.0; j <= i; j = j + 1.0) {
-		printf("Number %2.1f Sqaure Root %.6f\n", j, Sqrt(j, 0.000000001));
+		printf("Number %2.1f Sqaure Root %.*f\n", j,pre, Sqrt(j));
 	}
 	return 0;
 }
