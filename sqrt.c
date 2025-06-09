779aafde31b00d5d77bfc5cd0a8153a13a4f6d2d
yhypno <yashwantbora119@gmail.com>
2025-06-09 20:29:35 +0530

    feat: added header file

--- sqrt.c
+++ sqrt.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "sqrt.h"
 
 float Sqrt(float a)
 {
@@ -23,20 +24,3 @@ float Sqrt(float a)
 }
 
 
-int main()
-{
-	float i;
-
-	printf("Enter ending number number:(eg. 1.00, 10.0)\n");
-	scanf("%f",&i);
-
-	int pre;
-	printf("Enter the precision:(eg 4,5,6;)\n");
-	scanf("%d",&pre);
-	
-
-	for (float j = 1.0; j <= i; j = j + 1.0) {
-		printf("Number %2.1f Sqaure Root %.*f\n", j,pre, Sqrt(j));
-	}
-	return 0;
-}
